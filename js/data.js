9e601f59e365de105b6c80a178fb4795143171ed
xiongood <xiongood@88.com>
2024-05-11 09:54:13 +0000

    Adding mp3 via GitHub Actions

--- js/data.js
+++ js/data.js
@@ -1,70 +1,20 @@
 const mockMusicFiles = [
 	{
-		"name":"20240510直播：大失所望，习近平欧洲行空手而归；普京来不来中国态度暧昧，美欧等不及已开始动手；五一消费四大辛酸现象，ICU里的中国房地产，海关数据造假掩不住出口惨淡 。 第1470期",
+		"name":"大康有话说-20240510直播：大失所望，习近平欧洲行空手而归；普京来不来中国态度暧昧，美欧等不及已开始动手；五一消费四大辛酸现象，ICU里的中国房地产，海关数据造假掩不住出口惨淡 。 第1470期",
 		"url":"https://xiongood.github.io/copyYoutube/dakang_2024-05-10_2.mp3"
 	},
 	{
-		"name":"曾經叱咤風雲的電腦病毒，爲何近年來越來越少，很少聽説了？（我們一起聊科學：20240511 第48期）",
+		"name":"大康有话说-曾經叱咤風雲的電腦病毒，爲何近年來越來越少，很少聽説了？（我們一起聊科學：20240511 第48期）",
 		"url":"https://xiongood.github.io/copyYoutube/dakang_2024-05-10_1.mp3"
 	},
 	{
-		"name":"答网友问：王局的节目尺度有些回收？",
+		"name":"王局拍案-答网友问：王局的节目尺度有些回收？",
 		"url":"https://xiongood.github.io/copyYoutube/wangju_2024-05-10_4.mp3"
 	},
 	{
-		"name":"璩静何许人也？一句话让百度市值蒸发60亿",
+		"name":"王局拍案-璩静何许人也？一句话让百度市值蒸发60亿",
 		"url":"https://xiongood.github.io/copyYoutube/wangju_2024-05-10_3.mp3"
 	},
-	{
-		"name":"习近平称中方支持匈牙利在欧盟内发挥更大作用",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_15.mp3"
-	},
-	{
-		"name":"韩国无人机灯光秀",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_14.mp3"
-	},
-	{
-		"name":"05/10【时事大家谈】习近平欧洲三国行，最后成绩单如何？华盛顿制裁措施升级，为何华为能够一再突围？",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_13.mp3"
-	},
-	{
-		"name":"日防卫大臣誓言加强安保能力 因应无人机威胁",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_12.mp3"
-	},
-	{
-		"name":"菲律宾国家安全顾问呼吁驱逐中国外交官 民众怎么看",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_11.mp3"
-	},
-	{
-		"name":"报告：欧企对中国市场信心跌至新低",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_10.mp3"
-	},
-	{
-		"name":"中国出现准许民进党人士赴港的建议，密谈管道重开？",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_9.mp3"
-	},
-	{
-		"name":"时事大家谈：习近平欧洲三国行，最后成绩单如何？",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_8.mp3"
-	},
-	{
-		"name":"时事大家谈：华盛顿制裁措施升级，为何华为能够一再突围？",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_7.mp3"
-	},
-	{
-		"name":"波兰农民举行抗议欧盟绿色协议的示威游行",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_6.mp3"
-	},
-	{
-		"name":"北约在爱沙尼亚举行军事演习",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_5.mp3"
-	},
-	{
-		"name":"英国环保人士企图毁坏大宪章",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_4.mp3"
-	},
-	{
-		"name":"时事经纬(2024年5月11日)习近平跑趟欧洲，欧企信心更低了；习近平想重定国际格局，分析称“不可能”；中国打算把核反应堆浮海面，地区安全更悬了 - 5月 11日,2024年",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_3.mp3"
-	}
-];
+
+        ];
+        
