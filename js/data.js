08cf3d7ac18a3c78b6964561c1318cd99b402bc0
xiongood <xiongood@88.com>
2024-05-11 17:30:03 +0000

    Adding mp3 via GitHub Actions

--- js/data.js
+++ js/data.js
@@ -16,56 +16,28 @@ const mockMusicFiles = [
 		"url":"https://xiongood.github.io/copyYoutube/wangju_2024-05-10_3.mp3"
 	},
 	{
-		"name":"美国之音-习近平称中方支持匈牙利在欧盟内发挥更大作用",
+		"name":"美国之音-时事大家谈：习近平欧洲三国行，最后成绩单如何？",
 		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_15.mp3"
 	},
 	{
-		"name":"美国之音-韩国无人机灯光秀",
+		"name":"美国之音-时事大家谈：华盛顿制裁措施升级，为何华为能够一再突围？",
 		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_14.mp3"
 	},
 	{
-		"name":"美国之音-05/10【时事大家谈】习近平欧洲三国行，最后成绩单如何？华盛顿制裁措施升级，为何华为能够一再突围？",
+		"name":"美国之音-波兰农民举行抗议欧盟绿色协议的示威游行",
 		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_13.mp3"
 	},
 	{
-		"name":"美国之音-日防卫大臣誓言加强安保能力 因应无人机威胁",
+		"name":"美国之音-北约在爱沙尼亚举行军事演习",
 		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_12.mp3"
 	},
 	{
-		"name":"美国之音-菲律宾国家安全顾问呼吁驱逐中国外交官 民众怎么看",
+		"name":"美国之音-英国环保人士企图毁坏大宪章",
 		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_11.mp3"
 	},
 	{
-		"name":"美国之音-报告：欧企对中国市场信心跌至新低",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_10.mp3"
-	},
-	{
-		"name":"美国之音-中国出现准许民进党人士赴港的建议，密谈管道重开？",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_9.mp3"
-	},
-	{
-		"name":"美国之音-时事大家谈：习近平欧洲三国行，最后成绩单如何？",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_8.mp3"
-	},
-	{
-		"name":"美国之音-时事大家谈：华盛顿制裁措施升级，为何华为能够一再突围？",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_7.mp3"
-	},
-	{
-		"name":"美国之音-波兰农民举行抗议欧盟绿色协议的示威游行",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_6.mp3"
-	},
-	{
-		"name":"美国之音-北约在爱沙尼亚举行军事演习",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_5.mp3"
-	},
-	{
-		"name":"美国之音-英国环保人士企图毁坏大宪章",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_4.mp3"
-	},
-	{
 		"name":"美国之音-时事经纬(2024年5月11日)习近平跑趟欧洲，欧企信心更低了；习近平想重定国际格局，分析称“不可能”；中国打算把核反应堆浮海面，地区安全更悬了 - 5月 11日,2024年",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_3.mp3"
+		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_10.mp3"
 	},
 
         ];
