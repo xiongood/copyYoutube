06514a4d69fc5d3354469303fa08a61ee9439481
xiongood <168077001+xiongood@users.noreply.github.com>
2024-05-11 17:31:47 +0800

    Update data.js
    测试

--- js/data.js
+++ js/data.js
@@ -68,10 +68,6 @@ const mockMusicFiles = [
 		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_3.mp3"
 	},
     {
-        "name": "大康有话说_2024-05-10_1.mp3",
-        "url": "https://myfox.fun/copyYoutube/%E5%A4%A7%E5%BA%B7%E6%9C%89%E8%AF%9D%E8%AF%B4_2024-05-10_1.mp3"
-    },
-    {
         "name": "大康有话说_2024-05-10_2.mp3",
         "url": "https://myfox.fun/copyYoutube/%E5%A4%A7%E5%BA%B7%E6%9C%89%E8%AF%9D%E8%AF%B4_2024-05-10_1.mp3"
     }
