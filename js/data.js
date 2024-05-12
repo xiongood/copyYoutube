a5e8e7dd8a897c01fb8e0afb0a15a1be0eec4b09
xiongood <xiongood@88.com>
2024-05-12 03:51:58 +0000

    Adding mp3 via GitHub Actions

--- js/data.js
+++ js/data.js
@@ -2,26 +2,26 @@ const dataAll={
     "dataDate": "2024-05-11",
     "authors": [
         {
-            "name": "\u5927\u5eb7\u6709\u8bdd\u8bf4",
+            "name": "大康有话说",
             "dataList": []
         },
         {
-            "name": "\u738b\u5c40\u62cd\u6848",
+            "name": "王局拍案",
             "dataList": [
                 {
-                    "title": "\u738b\u5c40\u4e8c\u5927\u7237\u5bf9\u8c08\uff1a\u8d70\u5f97\u518d\u8fdc\uff0c\u4e5f\u8d70\u4e0d\u51fa\u53bb\u56fd\u6000\u4e61\uff1b",
+                    "title": "王局二大爷对谈：走得再远，也走不出去国怀乡；",
                     "url": "https://xiongood.github.io/copyYoutube/wangju_2024-05-11_3.mp3",
-                    "duration": "57\u520659\u79d2"
+                    "duration": "57分59秒"
                 }
             ]
         },
         {
-            "name": "\u7f8e\u56fd\u4e4b\u97f3",
+            "name": "美国之音",
             "dataList": [
                 {
-                    "title": "\u4e13\u8bbf\u519b\u53f2\u5b66\u5bb6\u6c49\u68ee\uff1a\u4e2d\u56fd\u662f\u4e4c\u6218\u7684\u6700\u5927\u8d62\u5bb6\uff1f",
-                    "url": "https://xiongood.github.io/copyYoutube/meiguo_2024-05-11_7.mp3",
-                    "duration": "49\u520657\u79d2"
+                    "title": "专访军史学家汉森：中国是乌战的最大赢家？",
+                    "url": "https://xiongood.github.io/copyYoutube/meiguo_2024-05-11_8.mp3",
+                    "duration": "49分57秒"
                 }
             ]
         }
