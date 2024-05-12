72a6ccf7701749f1848dd5134a0865ef922d0399
xiongood <xiongood@88.com>
2024-05-12 03:12:25 +0000

    Adding mp3 via GitHub Actions

--- js/data.js
+++ js/data.js
@@ -1,40 +1,29 @@
-const mockMusicFiles = [
-	{
-		"name":"王局拍案-王局二大爷对谈：走得再远，也走不出去国怀乡；",
-		"url":"https://xiongood.github.io/copyYoutube/wangju_2024-05-11_2.mp3"
-	},
-	{
-		"name":"王局拍案-福建舰海试，其技术已与美国平起平坐？",
-		"url":"https://xiongood.github.io/copyYoutube/wangju_2024-05-11_1.mp3"
-	},
-	{
-		"name":"美国之音-乌克兰哈尔科夫州长说激烈战斗仍在进行",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-11_8.mp3"
-	},
-	{
-		"name":"美国之音-专访军史学家汉森：中国是乌战的最大赢家？",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-11_7.mp3"
-	},
-	{
-		"name":"美国之音-【胡佛研究所军史学家汉森：习近平疯了才会攻台】5/11 #纵深视角 #精彩点评",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-11_6.mp3"
-	},
-	{
-		"name":"美国之音-05/11【鹰与盾】美菲军演击沉中制假想敌/美军新型两栖战车首登场",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-11_5.mp3"
-	},
-	{
-		"name":"美国之音-波兰总理承诺要加强东部边界的安全",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-11_4.mp3"
-	},
-	{
-		"name":"美国之音-【《杀戮与文化》作者汉森：中国孙子兵法难敌西方军事优势】5/11#纵深视角 #精彩点评",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-11_2.mp3"
-	},
-	{
-		"name":"美国之音-美国明尼苏达州议会大厦升起新州旗",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-11_1.mp3"
-	},
-
-        ];
-        
+{
+    "dataDate": "2024-05-11",
+    "authors": [
+        {
+            "name": "\u5927\u5eb7\u6709\u8bdd\u8bf4",
+            "dataList": []
+        },
+        {
+            "name": "\u738b\u5c40\u62cd\u6848",
+            "dataList": [
+                {
+                    "title": "\u738b\u5c40\u4e8c\u5927\u7237\u5bf9\u8c08\uff1a\u8d70\u5f97\u518d\u8fdc\uff0c\u4e5f\u8d70\u4e0d\u51fa\u53bb\u56fd\u6000\u4e61\uff1b",
+                    "url": "https://xiongood.github.io/copyYoutube/wangju_2024-05-11_2.mp3",
+                    "duration": "57\u520659\u79d2"
+                }
+            ]
+        },
+        {
+            "name": "\u7f8e\u56fd\u4e4b\u97f3",
+            "dataList": [
+                {
+                    "title": "\u4e13\u8bbf\u519b\u53f2\u5b66\u5bb6\u6c49\u68ee\uff1a\u4e2d\u56fd\u662f\u4e4c\u6218\u7684\u6700\u5927\u8d62\u5bb6\uff1f",
+                    "url": "https://xiongood.github.io/copyYoutube/meiguo_2024-05-11_7.mp3",
+                    "duration": "49\u520657\u79d2"
+                }
+            ]
+        }
+    ]
+}
