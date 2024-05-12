4eaa77a7d96f8f6ec80f168974293f4e6d370bc7
xiongood <xiongood@88.com>
2024-05-12 01:50:53 +0000

    Adding mp3 via GitHub Actions

--- js/data.js
+++ js/data.js
@@ -1,43 +1,39 @@
 const mockMusicFiles = [
 	{
-		"name":"大康有话说-20240510直播：大失所望，习近平欧洲行空手而归；普京来不来中国态度暧昧，美欧等不及已开始动手；五一消费四大辛酸现象，ICU里的中国房地产，海关数据造假掩不住出口惨淡 。 第1470期",
-		"url":"https://xiongood.github.io/copyYoutube/dakang_2024-05-10_2.mp3"
+		"name":"王局拍案-王局二大爷对谈：走得再远，也走不出去国怀乡；",
+		"url":"https://xiongood.github.io/copyYoutube/wangju_2024-05-11_2.mp3"
 	},
 	{
-		"name":"大康有话说-曾經叱咤風雲的電腦病毒，爲何近年來越來越少，很少聽説了？（我們一起聊科學：20240511 第48期）",
-		"url":"https://xiongood.github.io/copyYoutube/dakang_2024-05-10_1.mp3"
+		"name":"王局拍案-福建舰海试，其技术已与美国平起平坐？",
+		"url":"https://xiongood.github.io/copyYoutube/wangju_2024-05-11_1.mp3"
 	},
 	{
-		"name":"王局拍案-答网友问：王局的节目尺度有些回收？",
-		"url":"https://xiongood.github.io/copyYoutube/wangju_2024-05-10_4.mp3"
+		"name":"美国之音-乌克兰哈尔科夫州长说激烈战斗仍在进行",
+		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-11_8.mp3"
 	},
 	{
-		"name":"王局拍案-璩静何许人也？一句话让百度市值蒸发60亿",
-		"url":"https://xiongood.github.io/copyYoutube/wangju_2024-05-10_3.mp3"
+		"name":"美国之音-专访军史学家汉森：中国是乌战的最大赢家？",
+		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-11_7.mp3"
 	},
 	{
-		"name":"美国之音-时事大家谈：习近平欧洲三国行，最后成绩单如何？",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_15.mp3"
+		"name":"美国之音-【胡佛研究所军史学家汉森：习近平疯了才会攻台】5/11 #纵深视角 #精彩点评",
+		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-11_6.mp3"
 	},
 	{
-		"name":"美国之音-时事大家谈：华盛顿制裁措施升级，为何华为能够一再突围？",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_14.mp3"
+		"name":"美国之音-05/11【鹰与盾】美菲军演击沉中制假想敌/美军新型两栖战车首登场",
+		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-11_5.mp3"
 	},
 	{
-		"name":"美国之音-波兰农民举行抗议欧盟绿色协议的示威游行",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_13.mp3"
+		"name":"美国之音-波兰总理承诺要加强东部边界的安全",
+		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-11_4.mp3"
 	},
 	{
-		"name":"美国之音-北约在爱沙尼亚举行军事演习",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_12.mp3"
+		"name":"美国之音-【《杀戮与文化》作者汉森：中国孙子兵法难敌西方军事优势】5/11#纵深视角 #精彩点评",
+		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-11_2.mp3"
 	},
 	{
-		"name":"美国之音-英国环保人士企图毁坏大宪章",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_11.mp3"
-	},
-	{
-		"name":"美国之音-时事经纬(2024年5月11日)习近平跑趟欧洲，欧企信心更低了；习近平想重定国际格局，分析称“不可能”；中国打算把核反应堆浮海面，地区安全更悬了 - 5月 11日,2024年",
-		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-10_10.mp3"
+		"name":"美国之音-美国明尼苏达州议会大厦升起新州旗",
+		"url":"https://xiongood.github.io/copyYoutube/meiguo_2024-05-11_1.mp3"
 	},
 
         ];
