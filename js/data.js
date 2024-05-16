bf6d354e642cbf411117e3a9ff4a235924b0aa98
xiongood <xiongood@88.com>
2024-05-16 03:59:52 +0000

    Adding mp3 via GitHub Actions

--- js/data.js
+++ js/data.js
@@ -6,19 +6,19 @@ const dataAll={
             "dataList": [
                 {
                     "title": "20240515直播：拜登关税战！喊声大力道小侮辱性强，是否贸易战要看欧盟和中国；普京携3大筹码高调访华，又当又立的习近平左右为难。（20240515第1474期）",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/dakang_1.mp3",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/dakang_2.mp3",
                     "duration": "98分0秒",
                     "published": "2024-05-15T03:43:39+00:00"
                 },
                 {
                     "title": "20240514直播：国民党自绝于台湾？当年老蒋赌气铸成大错；新轴心已经成型，中俄结盟已到无可挽回的地步；真没辙了！发50年超长国债；4月金融数据证明政策根本无效。（20240514 第1473期）",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/dakang_2.mp3",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/dakang_3.mp3",
                     "duration": "88分0秒",
                     "published": "2024-05-14T03:33:18+00:00"
                 },
                 {
                     "title": "20240513直播：蔡英文是个好总统！中国大面积社会溃败的政治根源，回想当年的那场脆断；答观众问：中国的‘礼仪之邦’不是你想的那样，‘四大文明古国’都是瞎掰的。（20240513 第1472期）",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/dakang_3.mp3",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/dakang_4.mp3",
                     "duration": "100分44秒",
                     "published": "2024-05-13T03:47:26+00:00"
                 }
@@ -56,12 +56,6 @@ const dataAll={
                     "url": "https://xiongood.github.io/copyYoutube/mp3/wangju_5.mp3",
                     "duration": "3分17秒",
                     "published": "2024-05-13T08:20:24+00:00"
-                },
-                {
-                    "title": "擁有勇氣匹敵擁有權力？坦克人和毛澤東並駕齊驅",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/wangju_6.mp3",
-                    "duration": "0分59秒",
-                    "published": "2024-05-13T03:13:41+00:00"
                 }
             ]
         },
@@ -113,6 +107,126 @@ const dataAll={
                     "url": "https://xiongood.github.io/copyYoutube/mp3/shijiezhizuiTOP_1.mp3",
                     "duration": "23分40秒",
                     "published": "2024-05-14T09:00:21+00:00"
+                }
+            ]
+        },
+        {
+            "name": "寰宇新聞",
+            "dataList": []
+        },
+        {
+            "name": "文昭談古論今",
+            "dataList": [
+                {
+                    "title": "突發，直飛上海的航線被取消，重啟僅9個月！一個時代就這麼落幕了（文昭談古論今20240515第1411期）",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/wenzhao_1.mp3",
+                    "duration": "20分53秒",
+                    "published": "2024-05-16T00:32:46+00:00"
+                },
+                {
+                    "title": "習近平面前的黑色茶杯曝光，坐實重大謠言！兩個大戰逼近中共，拜登終於使出狠招（文昭談古論今20240513第1410期）",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/wenzhao_2.mp3",
+                    "duration": "24分27秒",
+                    "published": "2024-05-13T23:00:24+00:00"
+                }
+            ]
+        },
+        {
+            "name": "小Lin说",
+            "dataList": []
+        },
+        {
+            "name": "纪实杂谈",
+            "dataList": [
+                {
+                    "title": "生活按节奏来最离谱？但想按自己的节奏来，一般内驱力达不到《十三邀S3 ThirteenTalks》 #许知远#十三邀",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_1.mp3",
+                    "duration": "24分26秒",
+                    "published": "2024-05-15T14:00:07+00:00"
+                },
+                {
+                    "title": "人过五十岁，请过“不要脸”的生活，70岁老人的话不得不听《十三邀S6 ThirteenTalks》 #许知远#十三邀",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_2.mp3",
+                    "duration": "29分0秒",
+                    "published": "2024-05-15T13:30:32+00:00"
+                },
+                {
+                    "title": "所有关系越来越淡，往往源于这一点，30岁以后就必须面对的事情《十三邀S2 ThirteenTalks》 #许知远#十三邀",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_3.mp3",
+                    "duration": "27分32秒",
+                    "published": "2024-05-15T13:00:52+00:00"
+                },
+                {
+                    "title": "原来这样做事的人，一辈子都会被老天“折磨”，看完你就明白了！《十三邀S6 ThirteenTalks》 #许知远#十三邀",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_4.mp3",
+                    "duration": "29分26秒",
+                    "published": "2024-05-15T12:30:01+00:00"
+                },
+                {
+                    "title": "年龄越大长得越丑？灵气消失的重要3个迹象，看完张双南更懂恢复《十三邀S7 ThirteenTalks》 #许知远#十三邀",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_5.mp3",
+                    "duration": "29分20秒",
+                    "published": "2024-05-15T12:00:37+00:00"
+                },
+                {
+                    "title": "会说话就是嘴甜？完全相反，但会一定能说到点上《十三邀S6 ThirteenTalks》 #许知远#十三邀",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_6.mp3",
+                    "duration": "27分58秒",
+                    "published": "2024-05-14T14:00:09+00:00"
+                },
+                {
+                    "title": "当美国小孩接触中国生活后，赖声川三天没说话，突然一下就理解了《十三邀S5 ThirteenTalks》 #许知远#十三邀",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_7.mp3",
+                    "duration": "27分47秒",
+                    "published": "2024-05-14T13:30:15+00:00"
+                },
+                {
+                    "title": "一个人的觉醒，是从三观破裂开始的，重点在四个字！《十三邀S6 ThirteenTalks》 #许知远#十三邀",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_8.mp3",
+                    "duration": "28分30秒",
+                    "published": "2024-05-14T13:00:20+00:00"
+                },
+                {
+                    "title": "花20年去了海外旅游，我的价值观早变了！旅游几十个国家，最爱马来西亚《十三邀S1 ThirteenTalks》 #许知远#十三邀",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_9.mp3",
+                    "duration": "26分26秒",
+                    "published": "2024-05-14T12:30:05+00:00"
+                },
+                {
+                    "title": "管理的本质，就是“抢夺”别人的成果拿结果，听完大受震撼！《十三邀S2 ThirteenTalks》 #许知远#十三邀",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_10.mp3",
+                    "duration": "28分58秒",
+                    "published": "2024-05-14T12:00:46+00:00"
+                },
+                {
+                    "title": "金融危机爆发，反而更容易赚钱暴富？赶快把钱放到这些地方！《十三邀S7 ThirteenTalks》 #许知远#十三邀",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_11.mp3",
+                    "duration": "28分47秒",
+                    "published": "2024-05-13T14:00:19+00:00"
+                },
+                {
+                    "title": "不要太守规矩，不要太守规矩，不要太守规矩！《十三邀S4 ThirteenTalks》 #许知远#十三邀",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_12.mp3",
+                    "duration": "29分50秒",
+                    "published": "2024-05-13T13:30:12+00:00"
+                },
+                {
+                    "title": "冯小刚密语：承认睡过、收过钱、给过资源，但没做到最大那一步《十三邀S5 ThirteenTalks》 #许知远#十三邀",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_13.mp3",
+                    "duration": "20分40秒",
+                    "published": "2024-05-13T13:00:46+00:00"
+                },
+                {
+                    "title": "俗话说“英雄难过美人关”，那美人难过什么关？听听俞飞鸿就懂了《十三邀S1 ThirteenTalks》 #许知远#十三邀",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_14.mp3",
+                    "duration": "29分12秒",
+                    "published": "2024-05-13T12:30:38+00:00"
+                },
+                {
+                    "title": "穷人为了存100块钱能有多极端？但借钱才是关键！普通人对钱一定要有概念《十三邀S4 ThirteenTalks》 #许知远#十三邀",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_15.mp3",
+                    "duration": "29分51秒",
+                    "published": "2024-05-13T12:00:33+00:00"
                 }
             ]
         }
