621f33e97380495c1b2e5552973624f595e5a26c
xiongood <xiongood@88.com>
2024-05-19 17:45:58 +0000

    Adding mp3 via GitHub Actions

--- js/data.js
+++ js/data.js
@@ -1,26 +1,26 @@
 const dataAll={
-    "dataDate": "2024-05-19",
+    "dataDate": "2024-05-20",
     "authors": [
         {
             "name": "大康有话说",
             "dataList": [
                 {
-                    "title": "美國登月是世紀騙局嗎？中國嫦娥工程副總設計師直播中意外揭秘：沒找到阿波羅盆地？（我們一起聊科學：20240518 第49期）",
+                    "title": "20240519直播：天然气大管子落地，反美轴心确立，俄罗斯外交巨大胜利；习近平放史诗级四大招救市，真实目的：忽悠民众加杠杆接盘买房。（20240519第1477期）",
                     "url": "https://xiongood.github.io/copyYoutube/mp3/dakang_1.mp3",
+                    "duration": "95分5秒",
+                    "published": "2024-05-19T03:41:37+00:00"
+                },
+                {
+                    "title": "美國登月是世紀騙局嗎？中國嫦娥工程副總設計師直播中意外揭秘：沒找到阿波羅盆地？（我們一起聊科學：20240518 第49期）",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/dakang_2.mp3",
                     "duration": "13分27秒",
                     "published": "2024-05-17T22:02:11+00:00"
                 },
                 {
                     "title": "20240517直播：解读可能把中国拖进坟墓的中俄联合声明：全方位合作事实结盟，普京的小表情耐人寻味；房地产‘重大举措’上路，后果不堪设想。（20240517 第1476期）",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/dakang_2.mp3",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/dakang_3.mp3",
                     "duration": "85分15秒",
                     "published": "2024-05-17T03:31:26+00:00"
-                },
-                {
-                    "title": "20240516直播：普京带这个人访华意图明显，摆在习近平面前的四道坎；中共一内一外两个表态意味深长，彭博：政府当大房东！准备接盘中国房地产。（20240516 第1475期）",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/dakang_3.mp3",
-                    "duration": "94分35秒",
-                    "published": "2024-05-16T03:40:26+00:00"
                 }
             ]
         },
@@ -38,12 +38,6 @@ const dataAll={
                     "url": "https://xiongood.github.io/copyYoutube/mp3/wangju_2.mp3",
                     "duration": "2分24秒",
                     "published": "2024-05-17T01:00:16+00:00"
-                },
-                {
-                    "title": "美国对中国电动车开征100%惩罚性关税，这能拯救美国传统汽车产业么？｜拜登｜惩罚性关税｜贸易战｜中国电动车｜比亚迪｜北京车展｜新能源｜燃油车｜王局拍案20240516",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/wangju_3.mp3",
-                    "duration": "31分53秒",
-                    "published": "2024-05-16T12:30:17+00:00"
                 }
             ]
         },
@@ -55,16 +49,16 @@ const dataAll={
             "name": "脑洞乌托邦",
             "dataList": [
                 {
-                    "title": "驚人奇觀！秦始皇陵兵马俑竟然复活了！2006年陕西警方活捉兵马俑視頻曝光！",
+                    "title": "知名畫家86歲娶36歲嬌妻上熱搜！是真愛還是炒作？",
                     "url": "https://xiongood.github.io/copyYoutube/mp3/wutuo_1.mp3",
-                    "duration": "0分51秒",
-                    "published": "2024-05-18T12:30:10+00:00"
+                    "duration": "0分48秒",
+                    "published": "2024-05-19T12:30:18+00:00"
                 },
                 {
-                    "title": "時隔23年，劉嘉玲親述綁架案真相！驚人照片遭惡意曝光，揭開娛樂圈最骯髒的一面 [She's Xiaowu 小烏]",
+                    "title": "驚人奇觀！秦始皇陵兵马俑竟然复活了！2006年陕西警方活捉兵马俑視頻曝光！",
                     "url": "https://xiongood.github.io/copyYoutube/mp3/wutuo_2.mp3",
-                    "duration": "22分30秒",
-                    "published": "2024-05-16T12:30:19+00:00"
+                    "duration": "0分51秒",
+                    "published": "2024-05-18T12:30:10+00:00"
                 }
             ]
         },
@@ -72,10 +66,10 @@ const dataAll={
             "name": "崔永元",
             "dataList": [
                 {
-                    "title": "司马加持，哈马斯 普京 香飘飘了",
+                    "title": "不信谣 不传谣 我还在国内被封禁着",
                     "url": "https://xiongood.github.io/copyYoutube/mp3/cuiyongy_1.mp3",
-                    "duration": "11分28秒",
-                    "published": "2024-05-16T05:35:15+00:00"
+                    "duration": "20分50秒",
+                    "published": "2024-05-19T03:30:09+00:00"
                 }
             ]
         },
@@ -83,7 +77,7 @@ const dataAll={
             "name": "柴静",
             "dataList": [
                 {
-                    "title": "【柴静访谈美国华裔博士王夕越/妻子曲桦】“我如何把丈夫救出伊朗监狱？”",
+                    "title": "【柴静访谈前伊朗囚徒美国华裔博士王夕越/妻子曲桦】“我如何把丈夫救出伊朗监狱？”",
                     "url": "https://xiongood.github.io/copyYoutube/mp3/chaijing_1.mp3",
                     "duration": "51分18秒",
                     "published": "2024-05-18T13:00:14+00:00"
@@ -105,22 +99,22 @@ const dataAll={
             "name": "世界之最TOP",
             "dataList": [
                 {
-                    "title": "全世界最危險的10種糖果！放進嘴裡可能直接爆炸，第一名千萬別吃，否則後果自負。|#世界之最top #世界之最 #出類拔萃 #腦洞大開 #top10 #最危險糖果",
+                    "title": "全世界最貴的10個總統豪宅！美國只排第9，金正恩排名第5，第一名價值高達648億，卻沒人知道。|#世界之最top #世界之最 #出類拔萃 #腦洞大開 #top10 #總統豪宅 #金正恩",
                     "url": "https://xiongood.github.io/copyYoutube/mp3/shijiezhizuiTOP_1.mp3",
+                    "duration": "25分48秒",
+                    "published": "2024-05-19T09:00:14+00:00"
+                },
+                {
+                    "title": "全世界最危險的10種糖果！放進嘴裡可能直接爆炸，第一名千萬別吃，否則後果自負。|#世界之最top #世界之最 #出類拔萃 #腦洞大開 #top10 #最危險糖果",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/shijiezhizuiTOP_2.mp3",
                     "duration": "21分30秒",
                     "published": "2024-05-18T09:00:02+00:00"
                 },
                 {
                     "title": "非洲最可怕的10個驚人發現！探險隊冒死拍攝的恐怖畫面，第一名科學家至今無法解釋。|#世界之最top #世界之最 #出類拔萃 #腦洞大開 #top10 #非洲 #驚人發現",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/shijiezhizuiTOP_2.mp3",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/shijiezhizuiTOP_3.mp3",
                     "duration": "24分10秒",
                     "published": "2024-05-17T09:00:56+00:00"
-                },
-                {
-                    "title": "金正恩最強的10個安保秘密！40年沒有被刺殺成功，第一名堪稱“不死絕招”，連美國總統都羨慕。|#世界之最top #世界之最 #出類拔萃 #腦洞大開 #top10 #金正恩 #最強安保",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/shijiezhizuiTOP_3.mp3",
-                    "duration": "20分48秒",
-                    "published": "2024-05-16T09:00:36+00:00"
                 }
             ]
         },
@@ -136,12 +130,6 @@ const dataAll={
                     "url": "https://xiongood.github.io/copyYoutube/mp3/wenzhao_1.mp3",
                     "duration": "22分38秒",
                     "published": "2024-05-17T23:28:43+00:00"
-                },
-                {
-                    "title": "突發，直飛上海的航線被取消，重啟僅9個月！一個時代就這麼落幕了（文昭談古論今20240515第1411期）",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/wenzhao_2.mp3",
-                    "duration": "20分53秒",
-                    "published": "2024-05-16T00:32:46+00:00"
                 }
             ]
         },
@@ -160,94 +148,94 @@ const dataAll={
             "name": "纪实杂谈",
             "dataList": [
                 {
-                    "title": "不休息不结婚的香港人，平时都会干嘛？真实生活让人吃惊《十三邀S1 ThirteenTalks》 #许知远#十三邀",
+                    "title": "事实证明，90后的演员已经走进了一个“怪圈”，郝蕾全说对了《十三邀S5 ThirteenTalks》 #许知远#十三邀",
                     "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_1.mp3",
+                    "duration": "24分43秒",
+                    "published": "2024-05-19T14:00:16+00:00"
+                },
+                {
+                    "title": "金融危机改变命运的“薇娅”们，迎接全球危机后，逃不开的宿命《十三邀S4 ThirteenTalks》 #许知远#十三邀",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_2.mp3",
+                    "duration": "25分45秒",
+                    "published": "2024-05-19T13:30:04+00:00"
+                },
+                {
+                    "title": "为何说阳谋比阴谋更毒？明知是坑却不得不跳，这是需要付出的沉重代价《十三邀S6 ThirteenTalks》 #许知远#十三邀",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_3.mp3",
+                    "duration": "26分31秒",
+                    "published": "2024-05-19T13:00:51+00:00"
+                },
+                {
+                    "title": "好就是坏，坏就是好，难道不是不分是非吗？其实不然《十三邀S2 ThirteenTalks》 #许知远#十三邀",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_4.mp3",
+                    "duration": "27分32秒",
+                    "published": "2024-05-19T12:30:18+00:00"
+                },
+                {
+                    "title": "人到晚年以后，最怕的那一个人是谁？现在知道还不算晚《十三邀S7 ThirteenTalks》 #许知远#十三邀",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_5.mp3",
+                    "duration": "25分18秒",
+                    "published": "2024-05-19T12:00:42+00:00"
+                },
+                {
+                    "title": "不休息不结婚的香港人，平时都会干嘛？真实生活让人吃惊《十三邀S1 ThirteenTalks》 #许知远#十三邀",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_6.mp3",
                     "duration": "26分7秒",
                     "published": "2024-05-18T14:00:27+00:00"
                 },
                 {
                     "title": "3年内，不考虑买房了，为你揭秘首富的无数条路《十三邀S5 ThirteenTalks》 #许知远#十三邀",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_2.mp3",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_7.mp3",
                     "duration": "24分36秒",
                     "published": "2024-05-18T13:30:04+00:00"
                 },
                 {
                     "title": "这个人的话你也信？活该被忽悠瘸了，历史学家的话真的不可信？《十三邀S6 ThirteenTalks》 #许知远#十三邀",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_3.mp3",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_8.mp3",
                     "duration": "29分25秒",
                     "published": "2024-05-18T13:00:34+00:00"
                 },
                 {
                     "title": "“春宵一刻值千金”，很多人想歪了本意！下半句的重点全在这了《十三邀S7 ThirteenTalks》 #许知远#十三邀",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_4.mp3",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_9.mp3",
                     "duration": "28分46秒",
                     "published": "2024-05-18T12:30:03+00:00"
                 },
                 {
                     "title": "走到最后的朋友，不可能是发小？人到中年明白，事情有多离谱《十三邀S2 ThirteenTalks》 #许知远#十三邀",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_5.mp3",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_10.mp3",
                     "duration": "29分0秒",
                     "published": "2024-05-18T12:00:19+00:00"
                 },
                 {
                     "title": "这几个无意识的行为，最能暴露一个人的人品，注意看细节《十三邀S7 ThirteenTalks》 #许知远#十三邀",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_6.mp3",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_11.mp3",
                     "duration": "28分49秒",
                     "published": "2024-05-17T14:00:26+00:00"
                 },
                 {
                     "title": "债务到底有啥可怕的，借新还旧不就行了？困住人的不止是观念《十三邀S4 ThirteenTalks》 #许知远#十三邀",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_7.mp3",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_12.mp3",
                     "duration": "27分23秒",
                     "published": "2024-05-17T13:30:00+00:00"
                 },
                 {
                     "title": "道破人性，直指人心，丝毫不留情面最“毒”的十句话！《十三邀S4 ThirteenTalks》 #许知远#十三邀",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_8.mp3",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_13.mp3",
                     "duration": "26分44秒",
                     "published": "2024-05-17T13:00:51+00:00"
                 },
                 {
                     "title": "为什么劝你一定要努力工作？背后的原因很现实，许知远彻底绷不住了《十三邀S2 ThirteenTalks》 #许知远#十三邀",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_9.mp3",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_14.mp3",
                     "duration": "29分36秒",
                     "published": "2024-05-17T12:30:08+00:00"
                 },
                 {
                     "title": "去了趟以色列，感觉这种城市美得不可救药，回来三天没说话《十三邀S3 ThirteenTalks》 #许知远#十三邀",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_10.mp3",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_15.mp3",
                     "duration": "30分6秒",
                     "published": "2024-05-17T12:00:20+00:00"
-                },
-                {
-                    "title": "句句不提成功，句句都是暴富！人生下半场，该翻脸时就翻脸《十三邀S7 ThirteenTalks》 #许知远#十三邀",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_11.mp3",
-                    "duration": "28分19秒",
-                    "published": "2024-05-16T14:00:47+00:00"
-                },
-                {
-                    "title": "这一次，把北京和台北完整的放在一起，才知道有些差距《十三邀S3 ThirteenTalks》 #许知远#十三邀",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_12.mp3",
-                    "duration": "28分54秒",
-                    "published": "2024-05-16T13:30:10+00:00"
-                },
-                {
-                    "title": "真实的经济情况到底是什么？不理财你每年亏多少钱？这些道理普通人更应该明白《十三邀S4 ThirteenTalks》 #许知远#十三邀",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_13.mp3",
-                    "duration": "27分27秒",
-                    "published": "2024-05-16T13:00:22+00:00"
-                },
-                {
-                    "title": "面善心黑的人，会经常对你说这3句话，越早远离越好《十三邀S5 ThirteenTalks》 #许知远#十三邀",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_14.mp3",
-                    "duration": "26分27秒",
-                    "published": "2024-05-16T12:30:17+00:00"
-                },
-                {
-                    "title": "晚年最好的活法：不是打牌跳舞聚会，而是这一种《十三邀S5 ThirteenTalks》 #许知远#十三邀",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/jishizatan_15.mp3",
-                    "duration": "29分10秒",
-                    "published": "2024-05-16T12:00:37+00:00"
                 }
             ]
         }
