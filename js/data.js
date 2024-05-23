e425d8db97be1ecc1ca0d9b2bcde2279ea9170f9
xiongood <xiongood@88.com>
2024-05-23 01:06:16 +0000

    Adding mp3 via GitHub Actions

--- js/data.js
+++ js/data.js
@@ -1,47 +1,16 @@
 const dataAll={
-    "dataDate": "2024-05-22",
+    "dataDate": "2024-05-23",
     "authors": [
         {
             "name": "大康有话说",
-            "dataList": [
-                {
-                    "title": "20240521直播：全挑明了！从两岸到两国，解读赖清德强硬讲话；一次让人担心有人生还的空难，受益者叫伊塔巴；外资创30年最低，地产狂欢转瞬即逝，收入下跌政府钱包真瘪了。20240521 第1479期",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/dakang_1.mp3",
-                    "duration": "91分18秒",
-                    "published": "2024-05-21T03:38:05+00:00"
-                },
-                {
-                    "title": "20240519直播：天然气大管子落地，反美轴心确立，俄罗斯外交巨大胜利；习近平放史诗级四大招救市，真实目的：忽悠民众加杠杆接盘买房。（20240519第1477期）",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/dakang_3.mp3",
-                    "duration": "95分5秒",
-                    "published": "2024-05-19T03:41:37+00:00"
-                }
-            ]
+            "dataList": []
         },
         {
             "name": "王局拍案",
             "dataList": [
                 {
-                    "title": "赖清德就职演说推销“务实台独”，两岸关系风起云涌。｜民进党｜台湾总统｜赖清德｜萧美琴｜蔡英文｜台独｜布林肯｜武统｜习近平｜王局拍案20240521",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/wangju_1.mp3",
-                    "duration": "33分13秒",
-                    "published": "2024-05-21T12:00:08+00:00"
-                },
-                {
-                    "title": "蔡总统的最后一天，赖总统的第一天。",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/wangju_3.mp3",
-                    "duration": "11分35秒",
-                    "published": "2024-05-21T08:01:43+00:00"
-                },
-                {
-                    "title": "普京访华，两个威权者抱团取暖。｜习近平｜普京｜俄乌战争｜金正恩｜远东｜俄罗斯｜美国｜王局拍案20240520",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/wangju_4.mp3",
-                    "duration": "31分23秒",
-                    "published": "2024-05-20T12:00:27+00:00"
-                },
-                {
                     "title": "普京访华，中俄轴心正式形成？",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/wangju_5.mp3",
+                    "url": "https://xiongood.github.io/copyYoutube/mp3/wangju_7.mp3",
                     "duration": "6分41秒",
                     "published": "2024-05-20T08:42:10+00:00"
                 }
@@ -64,14 +33,7 @@ const dataAll={
         },
         {
             "name": "崔永元",
-            "dataList": [
-                {
-                    "title": "不信谣 不传谣 我还在国内被封禁着",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/cuiyongy_1.mp3",
-                    "duration": "20分50秒",
-                    "published": "2024-05-19T03:30:09+00:00"
-                }
-            ]
+            "dataList": []
         },
         {
             "name": "柴静",
@@ -87,20 +49,7 @@ const dataAll={
         },
         {
             "name": "文昭談古論今",
-            "dataList": [
-                {
-                    "title": "伊朗總統墜機身亡，習近平心驚；大事扎堆湧現，政治「硬著陸」要到來？520後的台灣什麼樣（文昭談古論今20240520第1413期）",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/wenzhao_1.mp3",
-                    "duration": "25分46秒",
-                    "published": "2024-05-20T23:31:16+00:00"
-                },
-                {
-                    "title": "中共進入歷史垃圾時間，你要忍多久？垃圾時間內會發生什麼？【會員樣片節選】",
-                    "url": "https://xiongood.github.io/copyYoutube/mp3/wenzhao_2.mp3",
-                    "duration": "4分52秒",
-                    "published": "2024-05-19T23:15:02+00:00"
-                }
-            ]
+            "dataList": []
         },
         {
             "name": "小Lin说",
